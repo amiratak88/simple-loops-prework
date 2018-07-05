2498bf1345199c137fcd9266c61cd8e7d43ef056
amiratak88@gmail.com <amiratak88@gmail.com>
2018-07-05 03:02:39 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -13,7 +13,7 @@ def loop_iterator(number_of_times)
   loop do 
     puts "Welcome to Flatiron School's Web Development Course!"
     i += 1
-    break if i == number_of_times - 1
+    break if i == number_of_times
   end
 end
 
